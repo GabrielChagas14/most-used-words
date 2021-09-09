28ea3612543390e0ad1c235fcd3e17c7de174c27
gabriel_chagas <gabriel.chagas@grupoviadigital.com>
2021-09-09 09:56:14 -0300

    Pill and Home components created

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        outlined
+        append-icon="mdi-send"
+        mutiple
+        chips
+        v-model="files"
+        @click:append-icon="processSubtitles"
+      >
+      </v-file-input>
+    </v-form>
+    <div class="pills">
+      <Pill
+        v-for="word in grupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill.vue";
+export default {
+  components: { Pill },
+  data: function() {
+    return {
+      files: [],
+      grupedWords: [
+        { name: "i", amount: 1860 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 860 },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ most-used-words/src/components/Pill.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pill">
+    <span class="name">{{ name }}</span>
+    <span class="amount">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style scoped>
+.pill {
+  background-color: rgb(230, 112, 180);
+  color: #fff;
+  margin: 10px;
+  height: 55px;
+  border-radius: 30px;
+  font-size: 1.3em;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+}
+.pill .name {
+  margin-left: 15px;
+}
+.pill .amount {
+  background: white;
+  margin: 10px;
+  padding: 5px;
+  height: 35px;
+  widows: 35px;
+  border-radius: 18px;
+  color: rgb(230, 112, 180);
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
